364d9651dc0e364014fa7eb77fa5e318c8b6f75e
Andy Bell <ambell@ucdavis.edu>
2014-08-21 13:57:03 -0700

    updated TIN_Display.py to create TIN from layer group rather than hardlinked inputs. All files must have the same Z field in order for it to work correctly. TODO: replace TIN_Display.py in arctool

--- TIN_Display_TESTER.py
+++ TIN_Display_TESTER.py
@@ -14,11 +14,23 @@ import os
 import arcpy.mapping
 from arcpy import env
 
+#check fieldnames match_field input
+def check_z(fc):
+    fieldList = arcpy.ListFields(fc)
+    check = False
+    for field in fieldList:
+        if field.name == z_field:
+            check = True
+    return check
+
+
 try:
     arcpy.CheckOutExtension("3D")
 
     #output from parameter
     output = arcpy.GetParameterAsText(0)
+    tin_in_group = arcpy.GetParameterAsText(1)
+    z_field = arcpy.GetParameterAsText(2)
 
     base = os.path.basename(output)
     arcpy.AddMessage(base)
@@ -26,10 +38,6 @@ try:
     # TIN inputs
     TINputs = "Bathymetry/channel_double NAVD88_m Mass_Points <None>;Bathymetry/H00935_t2r_10m NAVD88_m Mass_Points <None>;Bathymetry/manual_pts NAVD88_m Mass_Points <None>;Bathymetry/Small_parabolas_full NAVD88_m Mass_Points <None>;Bathymetry/Channel_edges_dice NAVD88_m Hard_Line <None>;Bathymetry/channel_lines_dice NAVD88_m Hard_Line <None>;Levees/Levee_Crests_NAVD88 Elevation_NAVD88_m Hard_Line <None>;Levees/Levee_footprints_split MSL_NAVD88 Hard_Line <None>"
     proj = "PROJCS['NAD_1983_UTM_Zone_10N',GEOGCS['GCS_North_American_1983',DATUM['D_North_American_1983',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',-123.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
-
-
-    #TIN symbology
-    symb = "U:/HDEM_v3r2/TINs/HDEM_SNs.lyr"
 
 
     #Get display extent from layout view (not sure if there is a better way since data view extent depends on the size of arcmap window)
@@ -43,9 +51,39 @@ try:
     arcpy.env.extent = display #set's the env extent to match the display extent. Not sure if tool with respect values!
 
 
+    #Get list of TIN input layers
+    tinputs = {} # create empty data dictionary
+
+    for layer in arcpy.mapping.ListLayers(IMXD):
+        if layer.name == tin_in_group:
+            arcpy.AddMessage("TIN inputs: ")
+            for subLayer in layer:
+                arcpy.AddMessage(subLayer)
+                if check_z(subLayer) == True:
+                    if arcpy.Describe(subLayer).shapeType == "Point":
+                        tinputs[subLayer.name] = "Mass_Points"
+                    elif arcpy.Describe(subLayer).shapeType == "Polyline":
+                        tinputs[subLayer.name] = "Hard_Line"
+                else:
+                    arcpy.AddError("Layer does not have a field named %s" %z_field)
+
+    # build Tin input format ("Group_Name/file_name z_field Type <None>;")
+    Tin_input_strings = []
+
+    for feature in tinputs:
+        feature_str = tin_in_group + "/" + feature + " " + z_field + " " + tinputs[feature] + " " + "<None>"
+        #arcpy.AddMessage(feature_str)
+        Tin_input_strings.append(feature_str)
+
+    Tin_input_str = ";".join(Tin_input_strings) # joins all individual strings together seperated by semicolon
+
+
     #Create TIN
-    arcpy.CreateTin_3d(output, proj, TINputs, "DELAUNAY")
+    arcpy.CreateTin_3d(output, proj, Tin_input_str, "DELAUNAY")
 
 except arcpy.ExecuteError:
 	print arcpy.GetMessages()
+
+
+
 
