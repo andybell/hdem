bb46f41a0d67bbde46f44296f8d841fe4443eb11
Andy Bell <ambell@ucdavis.edu>
2014-08-27 13:17:39 -0700

    mllw conversion only works for last element in list

--- conversion_mllw.py
+++ conversion_mllw.py
@@ -1,8 +1,8 @@
 #-------------------------------------------------------------------------------
 # Name: conversion_mllw.py
-# Purpose: converts points from mllw to NAVD88 based on tidal range and msl
+# Purpose: converts points from MLLW to NAVD88 based on a MLLW tidal datum surface
 # Author: Andy Bell (ambell@ucdavis.edu)
-# Created: 7/18/2014
+# Created: 8/27/2014
 #-------------------------------------------------------------------------------
 
 import arcpy
@@ -13,7 +13,7 @@ import exceptions, sys, traceback
 #check fieldnames for reserved names: "MSL_m" "Tidal_range_m" "NADV88m"
 def check_fieldnames(fc):
 	fieldList = arcpy.ListFields(fc)
-	reserved = ['MSL_m', 'Tidal_Range_m', 'NAVD88_m', 'Z', 'Z_MEAN', 'MLLW_m']
+	reserved = ['MSL_m', 'Tidal_Range_m', 'NAVD88_m', 'Z', 'Z_MEAN', 'WS_MLLW_m']
 	for field in fieldList:
 		if field.name in reserved:
 			return False
@@ -27,64 +27,55 @@ def change_fieldname(feature, oldfield, newfield):
 	arcpy.CalculateField_management (feature, newfield, '!' + oldfield + '!', "PYTHON_9.3", "#")
 	arcpy.DeleteField_management(feature, oldfield)
 
+
 try:
 	arcpy.CheckOutExtension("3D")
 	# Set Local Variables
-	env.workspace = arcpy.GetParameterAsText(0)
+	inString = arcpy.GetParameterAsText(0)
 	mllw_surface = arcpy.GetParameterAsText(1)
 
 	# get list of features
-	inString = arcpy.GetParameterAsText(0)
 	fcList = inString.split(";") #splits input string into a list of features
 
-	#arcpy.AddMessage(fcList)
+	arcpy.AddMessage(fcList)
 
 	# interate over feature list and change field names
 	# use extract values to points instead of addsurfaceinformation_3d
 	for feature in fcList:
 		arcpy.AddMessage(feature) #adds feature name to output
+        #feature type
+        desc = arcpy.Describe(feature)
+        fc_type = desc.shapeType
+        arcpy.AddMessage("Adding Mean Lower Low Water Field for water surface")
 
-		#checks if feature contains reserved field names
-		if check_fieldnames(feature) == False:
-			arcpy.AddError("Issue: fields already exist for feature. Delete problem fields and try again")
-			break
+        #Add surface information by input type
+        if fc_type == 'Point':
+            arcpy.AddSurfaceInformation_3d(feature, mllw_surface, "Z", "LINEAR")
+        elif fc_type == 'Polyline':
+            arcpy.AddSurfaceInformation_3d(feature, mllw_surface, "Z_MEAN", "LINEAR")
+            try:
+                arcpy.AlterField_management(feature, "Z_MEAN", "Z", "Z") # change field name to "Z"
+            except:
+                change_fieldname(feature, "Z_MEAN", "Z")
+        else:
+            arcpy.AddError("Issue: Feature type not supported. Only convert lines or points!")
 
-		#feature type
-		desc = arcpy.Describe(feature)
-		fc_type = desc.shapeType
-		arcpy.AddMessage(fc_type)
+        #change surface info field name
+        try: #attempts to change field using alter_field (arcgis v10.2.1+)
+            arcpy.AlterField_management(feature, "Z", "WS_MLLW_m", "WS_MLLW_m") # changes field name
+        except: #if alterfield does not work. Slower then alter field method for compatibility
+            change_fieldname(feature, "Z", "WS_MLLW_m")
 
-		#do stuff based on input type
-		if fc_type == 'Point':
+        #add field to calculate NAVD88 from sounding and mllw_m
+        arcpy.AddField_management(feature, "NAVD88_m", "DOUBLE")
+        arcpy.CalculateField_management(feature, "NAVD88_m", "!WS_MLLW_M! + !MLLW_m!", "PYTHON_9.3") #check!!
+        arcpy.AddMessage("Calculating NAVD88 Elevation: subtracting MLLW bed elevation from MLLW water surface")
 
-			#field with MSL
-			arcpy.AddMessage("Adding Mean Sea Level Surface Field")
-			arcpy.AddSurfaceInformation_3d(feature, msl_surface, "Z", "LINEAR")
-			try: #attempts to change field using alter_field (arcgis v10.2.1+)
-				arcpy.AlterField_management(feature, "Z", "MSL_m", "MSL_m") # changes field name
-			except: #if alterfield does not work. Slower then alter field method for compatibility
-				change_fieldname(feature, "Z", "MSL_m")
-
-
-		elif fc_type == 'Polyline':
-
-			#field with MSL
-			arcpy.AddMessage("Adding Mean Sea Level Surface Field")
-			arcpy.AddSurfaceInformation_3d(feature, msl_surface, "Z_MEAN", "LINEAR")
-			try: #attempts to change field using alter_field (arcgis v10.2.1+)
-				arcpy.AlterField_management(feature, "Z_MEAN", "MSL_m", "MSL_m") # changes field name
-			except: #if alterfield does not work. Slower then alter field method for compatibility
-				change_fieldname(feature, "Z_MEAN", "MSL_m")
-
-		else:
-			arcpy.AddError("Issue: Feature type not supported. Only convert lines or points!")
-			break
-
-		#add field to calculate NAVD88 for mllw_m, tidal_range_m and MSL_m
-		if str(MLLW2MSL_NAVD88) == 'true':
-			arcpy.AddField_management(feature, "NAVD88_m", "DOUBLE")
-			arcpy.CalculateField_management(feature, "NAVD88_m", "!MSL_m! + (!MLLW_m! - !Tidal_Range_m!/2)", "PYTHON_9.3") #check!!
-			arcpy.AddMessage("Calculating NAVD88 Elevation from 1/2 tidal range and mean sea level")
 
 except arcpy.ExecuteError:
 	print arcpy.GetMessages()
+
+
+"""#checks if feature contains reserved field names
+        if check_fieldnames(feature) == False:
+            arcpy.AddError("Issue: fields already exist for feature. Delete problem fields and try again")"""
