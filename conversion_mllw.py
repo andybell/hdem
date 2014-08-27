1bc721d4974514dc1f9687b23ae346e4724ac7b0
Andy Bell <ambell@ucdavis.edu>
2014-08-27 13:39:32 -0700

    fixed check_fieldnames

--- conversion_mllw.py
+++ conversion_mllw.py
@@ -14,12 +14,11 @@ import exceptions, sys, traceback
 def check_fieldnames(fc):
 	fieldList = arcpy.ListFields(fc)
 	reserved = ['MSL_m', 'Tidal_Range_m', 'NAVD88_m', 'Z', 'Z_MEAN', 'WS_MLLW_m']
+	check = True
 	for field in fieldList:
 		if field.name in reserved:
-			return False
-		else:
-			return True
-
+			check = False
+	return check
 
 ##### add -> copy -> delete field for arcgis versions <10.2.1 (alter_field does not exist)
 def change_fieldname(feature, oldfield, newfield):
@@ -37,43 +36,42 @@ try:
 	# get list of features
 	fcList = inString.split(";") #splits input string into a list of features
 
-	arcpy.AddMessage(fcList)
+	#arcpy.AddMessage(fcList)
 
 	# interate over feature list and change field names
 	# use extract values to points instead of addsurfaceinformation_3d
 	for feature in fcList:
-		arcpy.AddMessage(feature) #adds feature name to output
-		desc = arcpy.Describe(feature) #feature type
-		fc_type = desc.shapeType
-		arcpy.AddMessage("Adding Mean Lower Low Water Field for water surface")
+		if check_fieldnames(feature) == True:
+			arcpy.AddMessage(feature) #adds feature name to output
+			desc = arcpy.Describe(feature) #feature type
+			fc_type = desc.shapeType
+			arcpy.AddMessage("Adding Mean Lower Low Water Field for water surface")
 		
-		#Add surface information by input type
-		if fc_type == 'Point':
-			arcpy.AddSurfaceInformation_3d(feature, mllw_surface, "Z", "LINEAR")
-		elif fc_type == 'Polyline':
-			arcpy.AddSurfaceInformation_3d(feature, mllw_surface, "Z_MEAN", "LINEAR")
-			try:
-				arcpy.AlterField_management(feature, "Z_MEAN", "Z", "Z") # change field name to "Z"
-			except:
-				change_fieldname(feature, "Z_MEAN", "Z")
+			#Add surface information by input type
+			if fc_type == 'Point':
+				arcpy.AddSurfaceInformation_3d(feature, mllw_surface, "Z", "LINEAR")
+			elif fc_type == 'Polyline':
+				arcpy.AddSurfaceInformation_3d(feature, mllw_surface, "Z_MEAN", "LINEAR")
+				try:
+					arcpy.AlterField_management(feature, "Z_MEAN", "Z", "Z") # change field name to "Z"
+				except:
+					change_fieldname(feature, "Z_MEAN", "Z")
+			else:
+				arcpy.AddError("Issue: Feature type not supported. Only convert lines or points!")
+		
+			#change surface info field name
+			try: #attempts to change field using alter_field (arcgis v10.2.1+)
+				arcpy.AlterField_management(feature, "Z", "WS_MLLW_m", "WS_MLLW_m") # changes field name
+			except: #if alterfield does not work. Slower then alter field method for compatibility
+				change_fieldname(feature, "Z", "WS_MLLW_m")
+			#add field to calculate NAVD88 from sounding and mllw_m
+			arcpy.AddField_management(feature, "NAVD88_m", "DOUBLE")
+			arcpy.CalculateField_management(feature, "NAVD88_m", "!WS_MLLW_M! + !MLLW_m!", "PYTHON_9.3") #check!!
+			arcpy.AddMessage("Calculating NAVD88 Elevation: subtracting MLLW bed elevation from MLLW water surface")
 		else:
-			arcpy.AddError("Issue: Feature type not supported. Only convert lines or points!")
-		
-		#change surface info field name
-		try: #attempts to change field using alter_field (arcgis v10.2.1+)
-			arcpy.AlterField_management(feature, "Z", "WS_MLLW_m", "WS_MLLW_m") # changes field name
-		except: #if alterfield does not work. Slower then alter field method for compatibility
-			change_fieldname(feature, "Z", "WS_MLLW_m")
-		#add field to calculate NAVD88 from sounding and mllw_m
-		arcpy.AddField_management(feature, "NAVD88_m", "DOUBLE")
-		arcpy.CalculateField_management(feature, "NAVD88_m", "!WS_MLLW_M! + !MLLW_m!", "PYTHON_9.3") #check!!
-		arcpy.AddMessage("Calculating NAVD88 Elevation: subtracting MLLW bed elevation from MLLW water surface")
+			arcpy.AddError("Issue: fields already exist for feature. Delete problem fields and try again")
+			break
 
 
 except arcpy.ExecuteError:
 	print arcpy.GetMessages()
-
-
-"""#checks if feature contains reserved field names
-        if check_fieldnames(feature) == False:
-            arcpy.AddError("Issue: fields already exist for feature. Delete problem fields and try again")"""
