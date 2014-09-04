522fc74aa0967a4c5df2ee693ae2f79ddbd28f32
Andy Bell <ambell@ucdavis.edu>
2014-09-04 10:15:21 -0700

    changes in pycharm

--- TIN2ASCII.py
+++ TIN2ASCII.py
@@ -13,64 +13,64 @@ import os
 import glob
 import shutil
 
-from arcpy import env
-import exceptions, sys, traceback
-
 try:
-    #check out arcpy extensions.... if neeeded
-    arcpy.CheckOutExtension("3D")
+	#check out arcpy extensions.... if needed
+	arcpy.CheckOutExtension("3D")
 
-    #get parameters
-    tin = arcpy.GetParameterAsText(0)
-    output_folder = arcpy.GetParameterAsText(1)
-    rm_temp = arcpy.GetParameterAsText(2)
+	#get parameters
+	tin = arcpy.GetParameterAsText(0)
+	output_folder = arcpy.GetParameterAsText(1)
+	rm_temp = arcpy.GetParameterAsText(2)
 
-    sampling = "CELLSIZE 2"
-    ntiles = "3 5"  # tile dimensions
+	sampling = "CELLSIZE 2"
+	ntiles = "3 5"  # tile dimensions
 
-    #calculation of total number of tiles desired
-    spl = ntiles.split()
-    total_tiles = int(float(spl[0]))*int(float(spl[1]))
+	#calculation of total number of tiles desired
+	spl = ntiles.split()
+	total_tiles = int(float(spl[0]))*int(float(spl[1]))
 
-    #create temporary workspace
-    arcpy.AddMessage("Creating temporary folder in %s" %output_folder)
-    try:
-        os.makedirs(output_folder + "\\raster_tiles")
-        r_tiles = os.path.join(output_folder, "raster_tiles")
-        os.makedirs(output_folder + "\\ascii_tiles")
-        a_tiles = os.path.join(output_folder, "ascii_tiles")
-    except:
-        arcpy.AddMessage("Can't make folders")
+	#create temporary workspace
+	arcpy.AddMessage("Creating temporary folder in %s" % output_folder)
+	try:
+		os.makedirs(output_folder + "\\raster_tiles")
+		r_tiles = os.path.join(output_folder, "raster_tiles")
+		os.makedirs(output_folder + "\\ascii_tiles")
+		a_tiles = os.path.join(output_folder, "ascii_tiles")
+	except:
+		arcpy.AddError("Can't make folders")
 
-    #get tin basename
-    base = os.path.basename(tin)
-    arcpy.AddMessage("Saving %s as Raster..... " %base)
-    TinAsRast = os.path.join(output_folder, base + ".TIF")
+	#get tin basename
+	base = os.path.basename(tin)
+	arcpy.AddMessage("Saving %s as Raster..... " % base)
+	TinAsRast = os.path.join(output_folder, base + ".TIF")
 
-    #tin to raster
-    arcpy.AddMessage("Breaking %s into %s raster tiles....." %(base, total_tiles))
-    arcpy.TinRaster_3d(tin, TinAsRast, "FLOAT", "LINEAR", sampling)
-
-    #split raster into chunks
+	#tin to raster
+	arcpy.TinRaster_3d(tin, TinAsRast, "FLOAT", "LINEAR", sampling)
+	
+	#split raster into chunks
+	arcpy.AddMessage("Breaking %s into %s raster tiles....." % (base, total_tiles))
 	arcpy.SplitRaster_management(TinAsRast, r_tiles, base + "_", "NUMBER_OF_TILES", "TIFF", "BILINEAR", ntiles, '#', "10", "METERS")
 
-    #get list of split tiles
-    rastertiles_list = glob.glob(r_tiles + "/*.TIF")
+	#get list of split tiles
+	rastertiles_list = glob.glob(r_tiles + "/*.TIF")
+	arcpy.AddMessage(rastertiles_list)
 
-
-    for raster in rastertiles_list:
-		arcpy.AddMessage("Converting %s to ASCII....." %raster)
+	for raster in rastertiles_list:
+		arcpy.AddMessage("Converting %s to ASCII....." % raster)
 		rbase = os.path.basename(raster)
 		rbaseName, rbaseExt = os.path.splitext(rbase)
-		ascii_name = os.path.join(a_tiles, rbaseName + ".txt")
+		ascii_name = os.path.join(r_tiles, rbaseName + ".txt")
 		arcpy.RasterToASCII_conversion(raster, ascii_name)
 
-    # delete temporary scratch folder
-    arcpy.AddMessage(rm_temp)
-    if rm_temp == "true":
+	# delete temporary scratch folder
+	arcpy.AddMessage(rm_temp)
+	if rm_temp == "true":
 		arcpy.AddMessage("Removing Temporary Files....")
-		shutil.rmtree(r_tiles) #removes folder
-		arcpy.Delete_management(TinAsRast) # removes file
+		shutil.rmtree(r_tiles)  # removes folder
+		arcpy.Delete_management(TinAsRast)  # removes file
+	
+	#TODO: add option to zip files
+	# try using python module zipfile
 
 
 except arcpy.ExecuteError:
