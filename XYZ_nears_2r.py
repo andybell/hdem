49bee9036781cd44b8224702b0d1feeb6effed27
Andy Bell <ambell@ucdavis.edu>
2014-10-23 08:52:19 -0700

    transfer to r updates

--- XYZ_nears_2r.py
+++ XYZ_nears_2r.py
@@ -1,36 +1,49 @@
-# import arcpy
+# some usefull python snippets for coping near feature to r
 
-# purpose is to join the nearest channel and then the second nearest channel feature to the thalweg
-# 1. copy thalweg file to create a near bank
-#2. for near bank all we have to do is generate a near table and join the attribute to the original
-#4. copy thalweg file for far bank
-#5. for the far bank we need to search 180 degrees away from the near bank, locate closest and then join to original
-
-# original point on thalweg should have X1, Y1, Z1
-
-# add X2, Y2, Z2 for nearest point
-def addfields(target_table):
-	arcpy.AddField_management(target_table, "X2", "DOUBLE")
-	arcpy.AddField_management(target_table, "Y2", "DOUBLE")
-	arcpy.AddField_management(target_table, "Z2", "DOUBLE")
+# add double fields for items in the list
+def addfields(target_table, name_linst):
+	for name in name_list:
+		arcpy.AddField_management(target_table, name, "DOUBLE")
 
 
-# function to create the only the nearest feature -> should return a table that will be joined to the thalweg
-def join_nearest(target_feature, target_FID, near_features):
-	#need to create a temporary out_table?
-	near_table = r"in_memory\temp" # TODO: double check that this works!!!
-	arcpy.GenerateNearTable_analysis(target_feature, near_features, near_table, location="LOCATION",
-									angle="ANGLE", closest="CLOSEST")
-
-	arcpy.JoinField_management(target_feature, target_FID, near_table, "IN_FID", ["NEAR_X", "NEAR_Y", "NEAR_ANGLE"])
+def alter_fields(table, name_dict):
+	for key in name_dict:
+		arcpy.AlterField_management (table, name_dict[key], key, key)
 	
 	
-# generate near table with location and angle (50)?
-# modify field names to x2, y2, z2, 
-# join back to original fid to get x1,y1,z1
+# generate near table with location and angle and then join original fid to prime_feature coords
 # save as dbf for r to open???
+def near_table(prime_feature, near_features):
+	near_table = r"in_memory\temp" # TODO: double check that this works!!!
+	arcpy.GenerateNearTable_analysis(prime_feature, near_features, near_table, "500 Meters", location="LOCATION",
+									angle="ANGLE", closest="ALL", closest_count = 50)								
+	arcpy.JoinField_management(near_table, "IN_FID", prime_feature, "OBJECTID")
 
-arcpy.GenerateNearTable_analysis(target_feature, near_features, near_table, "500 Meters", location="LOCATION",
-									angle="ANGLE", closest="ALL", closest_count = "50)
-									
-arcpy.JoinField_management(near_table, "IN_FID", target_feature, "OBJECTID", ["X1", "Y1", "Z1"])
+
+# delete unneccesary fields
+def keep_mandatory_fields(table):
+	mandatory_field_list = ["OBJECTID", "X1", "Y1", "Z1", "X2", "Y2", "Z2", "NEAR_DIST", "NEAR_RANK", "NEAR_ANGLE"]
+	field_list = arcpy.ListFields(table)
+	for field in field_list:
+		print field.name
+		if field.name in mandatory_field_list:
+			pass
+		else:
+			arcpy.DeleteField_management(table, field.name)
+		
+	
+	
+# Example for whiskey slough!!!!
+# python dictionary with name change, new values should be the key and old the vaules
+whiskey = ''
+channels = ''
+
+whiskey_dict = {"X1":"ET_X", "Y1":"ET_Y", }
+
+alter_fields(whiskey, whiskey_dict)
+
+near_table(whiskey, channels)
+
+
+
+
