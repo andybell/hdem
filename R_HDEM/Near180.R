f52e9114d036c60ece1b4fcc9f6ee6067d211629
ambell <bell.andy.m@gmail.com>
2014-11-21 10:54:16 -0800

    near180 code works, takes two arguments (input near table in txt format and output location)

--- R_HDEM/Near180.R
+++ R_HDEM/Near180.R
@@ -3,8 +3,7 @@
 # subproccess module.
 
 #need to change number of digits so XY coordinates don't get cut off
-options(digits=16)
-
+options(scipen=100, digits=18)
 require(dplyr)
 require(plyr)
 
@@ -14,75 +13,41 @@ neg_angle<-function(angle){
   if (angle360 <= 180){angle360}
   else{angle360%%-180}}
 
-
 # find the row with the minimum near_distance
 nearest <- function(df){
   minima_row <- min(which(df$NEAR_DIST == min(df$NEAR_DIST)))
   return (df[minima_row,])
 }
 
-
 # find the row that has the lowest near distance opposite from the closest point
 nearest_opposite <- function(df){
   first<-nearest(df)
   first_angle <- first[1, "NEAR_ANGLE"]
-  limit1 <- neg_angle(first_angle+135)
-  limit2 <- neg_angle(first_angle+225)
+  limit1 <- neg_angle(first_angle+157.5)
+  limit2 <- neg_angle(first_angle+202.5)
   search_180 <- filter(df, if(limit1>limit2){NEAR_ANGLE > limit1 | NEAR_ANGLE < limit2} else{NEAR_ANGLE < limit2 & NEAR_ANGLE > limit1})
   second <-nearest(search_180)
   return(second)
 }
 
 
-# writes out df to a table. IDL needs comma seperated but saved as a .txt file 
-export_table <- function(df){
-  write.table(df, file = file.choose(), sep = ",", quote = FALSE, row.names=F)
-}
+####################################################################################
 
-# add zeros for Z2 -- waterline at MLLW
-add_z2 <- function(df){
-  df["Z2"]<-0
-  df
-}
-
-# drops unneccessary fields from the files and reorgizes them correctly
-# only want to keep OBJECTID, X1, Y1, Z1, X2, Y2, Z2
-drop_fields <- function(df){
-  fields = c("OBJECTID", "X1", "Y1", "Z1", "X2", "Y2", "Z2")
-  df <- subset(df, select = fields)
-}
-
-######
-######
-######
-######
-######
-
-
+#read in near table from command arguments as near_file
 near_file<-"C:\\Users\\ambell.AD3\\Documents\\hdem\\Near_Table_Example_comma.txt"
-
+location<-"C:\\Users\\ambell.AD3\\Documents\\hdem\\export_test.txt"
 near_table<-read.csv(near_file, header=TRUE, sep=",")
 
-
-
-# read dbf file in list of files
-nears<-read.dbf(files[i])
-
-# add column for Z2
-add_z2(nears)->nears
-
-
 ### use ddply to apply function(s) by IN_FID groups
-nearest_vertex<-ddply(nears, "IN_FID", nearest)
-opposite_vertex<-ddply(nears, "IN_FID", nearest_opposite)
+nearest_vertex<-ddply(near_table, "IN_FID", nearest)
+opposite_vertex<-ddply(near_table, "IN_FID", nearest_opposite)
 
 # rm NAs from opposites (no points within the 180 degree search area)
 opposite_vertex <-opposite_vertex[complete.cases(opposite_vertex),]
 
-#drop all unnecessary fields from nearest_vertex and opposite vertex dframes
-
-drop_fields(nearest_vertex)->nearest_vertex
-drop_fields(opposite_vertex)->opposite_vertex
-
 # append files to modifications file
-modifications_bin <-rbind(modifications_bin, nearest_vertex, opposite_vertex) # then just append using rbind to mod_bin
+export_bin <-rbind(nearest_vertex, opposite_vertex) # then just append using rbind
+
+#write table out to csv so that python can read it???
+write.table(export_bin, location, sep = ",", quote = FALSE, row.names=F)
+
