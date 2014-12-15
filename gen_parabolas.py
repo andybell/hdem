dbebcbd1a7abfdc085f68fe6f18d5f6c5f933184
ambell <bell.andy.m@gmail.com>
2014-12-15 14:36:35 -0800

    gen_parabolas streamline batch make points from known thalweg and near banks

--- gen_parabolas.py
+++ gen_parabolas.py
@@ -3,50 +3,122 @@ __author__ = 'ambell'
 import arcpy
 import parabola
 import csv
+import os
+import subprocess
+import tempfile
+import shutil
 
-# TODO make inNearTable and out_txt file parameter inputs from cmd!!!!
-# input near table with values for x sections with thalweg and bank Z's joined
-inNearTable = r"X:\delta_dem\HDEM_v5r1\SJ_con_example.gdb\Far_parabola"
-
-# Destination Feature Class for the points
-# Feature MUST have a field called "MLLW_m" as double
-out_txt = r"U:\HDEM_v5r1\SJ_convey_far_pts.txt"
+def addfields(target_table, name_list):
+	for name in name_list:
+		arcpy.AddField_management(target_table, name, "DOUBLE")
 
 
-#parse fields
-fields = ['FROM_X', 'FROM_Y', 'THALWEG_Z', 'NEAR_X', 'NEAR_Y', 'BANK_Z']
+def nearTable(thalweg_pts, channel_pts):
+	target_dbf = os.path.join(dirpath, "Input_Near_Table.dbf")
+	arcpy.GenerateNearTable_analysis(thalweg_pts, channel_pts, target_dbf, "#",
+	                                 "LOCATION", "ANGLE", "ALL", 200)
 
-end_pt_list = []
 
-print "Reading rows from the Near Table....."
-with arcpy.da.SearchCursor(inNearTable, fields) as cursor:
-	for row in cursor:
-		thalweg = (row[0], row[1], row[2])
-		bank = (row[3], row[4], row[5])
-		pair = (thalweg, bank)
-		end_pt_list.append(pair)
-del cursor
+def join_z_neartable(near_dbf, target_features, depth_field):
 
-counter = 1
+	#Add field for join (THALWEG_Z and BANK_Z as doubles)
+	new_fields = ["THALWEG_Z", "BANK_Z"]
+	addfields(near_dbf, new_fields)
 
-#export to csv/txt and then add points via arc xy to points tool
-txtfile = open(out_txt, 'w')
-writer = csv.writer(txtfile)
+	#join on thalweg_pts unique IDs
+	arcpy.JoinField_management(near_dbf, "IN_FID", target_features, "OBJECTID")
 
-rows = []
+	#Calculate fields
+	arcpy.CalculateField_management(near_dbf, "THALWEG_Z", '!' + depth_field + '!', "PYTHON_9.3")
 
-for point in end_pt_list:
-	print "Working on X section: %s" %counter
-	parabola_points = parabola.gen_pts(point[0], point[1])
-	counter += 1
+	#remove join by dropping unwanted fields
+	fields2keep = ["OID", "THALWEG_Z", "BANK_Z", "IN_FID", "NEAR_FID", "NEAR_DIST", "NEAR_RANK", "FROM_X", "FROM_Y", "NEAR_X", "NEAR_Y", "NEAR_ANGLE"]
 
-	for new_point in parabola_points:
-		row = [new_point[0], new_point[1], new_point[2]]
-		rows.append(row)
+	fields = arcpy.ListFields(near_dbf)
 
-writer.writerows(rows)
-txtfile.close()
+	for field in fields:
+		if field.name in fields2keep:
+			pass
+		else:
+			arcpy.DeleteField_management(near_dbf, field.name)
 
-#TODO: XY to points in acrpy?
 
-print "Finished!!!!!"
+def near180_subprocess(dirpath):
+
+	#location of R output dbf file
+	input_dbf = os.path.join(dirpath, "Input_Near_Table.dbf")
+	near180 = r"C:\Users\ambell.AD3\Documents\hdem\R_HDEM\Near180.R" # TODO change to be universal?
+	rscript_path = r"C:\Users\ambell.AD3\Documents\R\R-3.1.2\bin\rscript.exe" # TODO universal?
+
+
+	print "Calling {} {} --args {} {}".format(rscript_path, near180, input_dbf, dirpath)
+	#Subprocess call out to R to run Near180.R functions to reduce near table to two closest records
+	subprocess.call([rscript_path, near180, "--args", input_dbf, dirpath])
+
+
+def gen_pts_nears(inNearTable_withZ, out_directory):
+	fields = ['FROM_X', 'FROM_Y', 'THALWEG_Z', 'NEAR_X', 'NEAR_Y', 'BANK_Z']
+
+	end_pt_list = []  # empty list to store points
+
+	print "Reading rows from the Near Table....."
+	with arcpy.da.SearchCursor(inNearTable_withZ, fields) as cursor:
+		for row in cursor:
+			thalweg = (row[0], row[1], row[2])
+			bank = (row[3], row[4], row[5])
+			pair = (thalweg, bank)
+			end_pt_list.append(pair)
+	del cursor
+
+	#export to csv/txt and then add points via arc xy to points tool
+	out_txt = os.path.join(out_directory, "parabola_points.txt") # TODO: need separate files for near + opposite?
+	txtfile = open(out_txt, 'w')
+	writer = csv.writer(txtfile)
+
+	rows = []
+	counter = 1
+
+	for point in end_pt_list:
+		if counter % 100 == 0:
+			print "Working on X section: %s" % counter
+		parabola_points = parabola.gen_pts(point[0], point[1])
+		counter += 1
+
+		for new_point in parabola_points:
+			row = [new_point[0], new_point[1], new_point[2]]
+			rows.append(row)
+
+	writer.writerows(rows)
+	txtfile.close()
+
+
+def xy_to_pts(xy_txt, output):
+	arcpy.MakeXYEventLayer_management(xy_txt)
+	pass
+
+
+#Tester files
+thalweg_pts = r"U:\HDEM_v5r1_120914\Suisun_working_v5r1.gdb\Suisun_small_channel_soundings_h00948"
+banks_as_pts = r"U:\HDEM_v5r1_120914\Channel_pts_5m_GME_no_dups.shp"
+
+
+
+#make temp directory to store all interim data steps
+dirpath = tempfile.mkdtemp()
+
+
+
+print "Finding lots of nearest features....."
+nearTable(thalweg_pts, banks_as_pts)
+
+print "Finding two closest features on opposite banks (Near180.r)..."
+near180_subprocess(dirpath)
+
+print "Joining thalweg depths...."
+join_z_neartable(os.path.join(dirpath, "nearest_bank.dbf"), thalweg_pts, "MLLW_m")
+join_z_neartable(os.path.join(dirpath, "opposite_bank.dbf"), thalweg_pts, "MLLW_m")
+
+print "Generating parabola POINTS!!!"
+gen_pts_nears(os.path.join(dirpath, "nearest_bank.dbf"), dirpath)
+
+print "Temporary Directory: %s" % dirpath
--- pt_xsection.py
+++ pt_xsection.py
@@ -17,11 +17,10 @@ import os
 dirpath = tempfile.mkdtemp()
 
 # convert near table to dbf since R can't read the gdb table (stupid esri)
-arcpy.TableToTable_conversion(table, dirpath, "Near_table.dbf")
+arcpy.TableToTable_conversion(table, dirpath, "Input_Near_table.dbf")
 
 #location of R output dbf file
-input_dbf = os.path.join(dirpath, "Near_table.dbf")
-#output_dbf = os.path.join(dirpath, "Near180_table.dbf")  # change to output folder location once R script is modified
+input_dbf = os.path.join(dirpath, "Input_Near_Table.dbf")
 near180 = r"C:\Users\ambell.AD3\Documents\hdem\R_HDEM\Near180.R" # TODO change to be universal?
 rscript_path = r"C:\Users\ambell.AD3\Documents\R\R-3.1.2\bin\rscript.exe" # TODO universal?
 
@@ -30,7 +29,16 @@ print "Calling {} {} --args {} {}".format(rscript_path, near180, input_dbf, dirp
 #Subprocess call out to R to run Near180.R functions to reduce near table to two closest records
 subprocess.call([rscript_path, near180, "--args", input_dbf, dirpath])
 
+
 #TODO add field for join (THALWEG_Z and BANK_Z as doubles)
+def addfields(target_table, name_list):
+	for name in name_list:
+		arcpy.AddField_management(target_table, name, "DOUBLE")
+
+new_fields = ["THALWEG_Z", "BANK_Z"]
+
+addfields(os.path.join(dirpath, "nearest_bank.dbf"), new_fields)
+addfields(os.path.join(dirpath, "opposite_bank.dbf"), new_fields)
 
 # remove the temporary directory
 # shutil.rmtree(dirpath)
