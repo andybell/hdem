09bfad3b56c165b8c5ae59e732e061d1c8bddb9c
Andy Bell <ambell@ucdavis.edu>
2016-07-06 17:41:48 -0400

    fixed path to 3.2.3 r

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ __author__ = 'Andy'
 # path that need to be set in order to call R from python
 
 # path to R exe
-rscript_path = r"C:\Program Files\R\R-3.1.2\bin\rscript.exe"
+rscript_path = r"C:\Program Files\R\R-3.2.3\bin\rscript.exe"
 
 # path to R libraries
-r_lib_loc = r"C:\Users\Andy\Documents\R\win-library\3.1"
+r_lib_loc = r"C:\Users\Andy\Documents\R\win-library\3.2"
 
